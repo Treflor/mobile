965ba2de172a4182a0bf8275bba2e93e213f1b2e
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-09-25 12:32:36 +0530

    simple test for get my location

--- lib/screens/start/start_screen.dart
+++ lib/screens/start/start_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class StartScreen extends StatefulWidget {
   @override
@@ -6,12 +9,60 @@ class StartScreen extends StatefulWidget {
 }
 
 class _StartScreenState extends State<StartScreen> {
+  List<Position> positions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("Start"),
+        child: positions.isNotEmpty
+            ? ListView.builder(
+                itemCount: positions.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        "latitude: ${positions[index].latitude}, logitude: ${positions[index].longitude}"),
+                  );
+                },
+              )
+            : Text("not loaded"),
       ),
     );
+  }
+
+  void initLocation() async {
+    var geolocator = Geolocator();
+    var locationOptions =
+        LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+
+    StreamSubscription<Position> positionStream = geolocator
+        .getPositionStream(locationOptions)
+        .listen((Position position) {
+      print(position.toString());
+      print("from subscription");
+      setState(() {
+        this.positions.add(position);
+      });
+    });
+
+    print("position.toString()");
+    Position position = await Geolocator()
+        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position.toString());
+    setState(() {
+      this.positions.add(position);
+    });
+    position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position.toString());
+    setState(() {
+      this.positions.add(position);
+    });
   }
 }
