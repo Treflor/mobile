2587989d8868fdba90bde8208cdb7515c546ee57
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-09-26 20:55:33 +0530

    routings

---
+++ lib/routes/route_handler.dart
@@ -0,0 +1,21 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+
+import 'package:treflor/screens/main_screen.dart';
+import 'package:treflor/screens/auth/login_screen.dart';
+import 'package:treflor/screens/profile/profile.dart';
+
+var mainHandler = Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  return MainScreen();
+});
+
+var loginHandler = Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  return LoginScreen();
+});
+
+var profileHandler = Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  return ProfileScreen();
+});
---
+++ lib/treflor_app.dart
@@ -0,0 +1,29 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:treflor/state/config_bloc.dart';
+import 'routes/application.dart';
+import 'routes/routes.dart';
+
+class TreflorApp extends StatelessWidget {
+  TreflorApp() {
+    final router = Router();
+    Routes.configureRouter(router);
+    Application.router = router;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ConfigBLoC configBLoC = Provider.of<ConfigBLoC>(context);
+    return MaterialApp(
+      title: "Treflor",
+      theme: ThemeData(
+        brightness: configBLoC.darkMode ? Brightness.dark : Brightness.light,
+      ),
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: Application.router.generator,
+    );
+  }
+}
