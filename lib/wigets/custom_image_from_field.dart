9b7921517c12525e547196797c661d46331c2b67
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-09-28 15:52:42 +0530

    select profile picture from camera and gallery

--- lib/wigets/custom_image_from_field.dart
+++ lib/wigets/custom_image_from_field.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 
 class CustomImageFormField extends FormField<File> {
@@ -17,33 +18,72 @@ class CustomImageFormField extends FormField<File> {
           validator: validator,
           initialValue: initialValue,
           builder: (FormFieldState<File> state) {
-            return InkWell(
-              borderRadius: BorderRadius.circular(100),
-              child: state.value == null
-                  ? Hero(
-                      child: ClipOval(
-                        child: Image.asset(
-                          "assets/images/profile.jpg",
+            return Column(
+              children: <Widget>[
+                state.value == null
+                    ? Hero(
+                        child: ClipOval(
+                          child: Image.asset(
+                            "assets/images/profile.jpg",
+                            fit: BoxFit.cover,
+                            width: width,
+                            height: height,
+                          ),
+                        ),
+                        tag: "profile-pic",
+                      )
+                    : ClipOval(
+                        child: Image.file(
+                          state.value,
                           fit: BoxFit.cover,
                           width: width,
                           height: height,
                         ),
                       ),
-                      tag: "profile-pic",
-                    )
-                  : ClipOval(
-                      child: Image.file(
-                        state.value,
-                        fit: BoxFit.cover,
-                        width: width,
-                        height: height,
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white30,
+                      borderRadius: BorderRadius.circular(15)),
+                  padding: EdgeInsets.all(3),
+                  child: Wrap(
+                    direction: Axis.horizontal,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 15,
                       ),
-                    ),
-              onTap: () async {
-                var file =
-                    await ImagePicker.pickImage(source: ImageSource.gallery);
-                state.didChange(file);
-              },
+                      IconButton(
+                        onPressed: () async {
+                          var file = await ImagePicker.pickImage(
+                              source: ImageSource.camera);
+                          state.didChange(file);
+                        },
+                        icon: Icon(
+                          FontAwesomeIcons.camera,
+                          size: 26,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          var file = await ImagePicker.pickImage(
+                              source: ImageSource.gallery);
+                          state.didChange(file);
+                        },
+                        icon: Icon(
+                          FontAwesomeIcons.photoVideo,
+                          size: 26,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             );
           },
         );
