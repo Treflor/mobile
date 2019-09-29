4c9c1f288b1d5552d9287d2f57f7709ee91a6144
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-09-29 23:25:31 +0530

    bug fix

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,16 +1,9 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:treflor/state/config_state.dart';
-import 'package:treflor/state/auth_state.dart';
 
-class SettingsScreen extends StatefulWidget {
-  @override
-  _SettingsScreenState createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
+class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ConfigState configBLoC = Provider.of<ConfigState>(context);
@@ -27,7 +20,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
               title: Text("Dark Mode"),
               onTap: () => configBLoC.toggleDarkMode(),
             ),
-            Divider(),
           ],
         ),
       ),
