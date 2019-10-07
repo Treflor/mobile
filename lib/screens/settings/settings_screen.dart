d979c3384e36d7ed842b68102f23b804580fe180
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-10-07 20:59:33 +0530

    Added and updated about page

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:treflor/screens/settings/about/about.dart';
 import 'package:treflor/state/config_state.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -42,14 +43,20 @@ class SettingsScreen extends StatelessWidget {
           ),
           ListTile(
             leading: Icon(Icons.report),
-            title: Text('About')
+            title: Text('About'),
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => AboutScreen())
+              );
+            },
           ),
           // Padding(padding: EdgeInsets.only(top: 300.0),),
             Divider(),
             ListTile(
               leading: Icon(FontAwesomeIcons.signOutAlt),
               title: Text("Sign out"),
-              onTap: () => authState.signout(),
+              // onTap: () => authState.signout(),
             ),
           ],
         ),
