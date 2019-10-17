4624a25081da43cbe407c109a68947bda2f988ed
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-10-17 22:21:48 +0530

    Initial native android app

--- lib/screens/start/jpurney_bloc/bloc.dart
+++
@@ -1,3 +0,0 @@
-export 'journey_bloc.dart';
-export 'journey_event.dart';
-export 'journey_state.dart';
--- lib/screens/start/my_location_bloc/index.dart
+++
@@ -1,3 +0,0 @@
-export 'my_location_bloc.dart';
-export 'my_location_state.dart';
-export 'my_location_event.dart';
