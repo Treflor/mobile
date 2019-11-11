0b59525c12b76935d194092ce329675a3744ffef
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-11-12 03:05:55 +0530

    jwt provider

--- app/src/main/java/com/treflor/TreflorApplication.kt
+++ app/src/main/java/com/treflor/TreflorApplication.kt
@@ -1,13 +1,21 @@
 package com.treflor
 
 import android.app.Application
+import com.treflor.data.provider.JWTProvider
+import com.treflor.data.provider.JWTProviderImpl
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
 
 class TreflorApplication:Application(),KodeinAware {
 
     override val kodein: Kodein = Kodein.lazy {
-        //TODO bind things
+        import(androidXModule(this@TreflorApplication))
+
+        bind<JWTProvider>() with singleton { JWTProviderImpl(instance()) }
     }
 
 }
