db33fa01f2bb44230a2c7c011b5bb830e95fc29b
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-11-23 09:25:43 +0530

    sign up page upadated

--- app/src/main/java/com/treflor/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/treflor/ui/signup/SignUpFragment.kt
@@ -7,9 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 
 import com.treflor.R
 import kotlinx.android.synthetic.main.sign_up_fragment.*
+import java.util.*
 
 class SignUpFragment : Fragment() {
 
@@ -25,10 +27,17 @@ class SignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        gender_spinner.adapter = ArrayAdapter<String>(
+        gender_spinner.adapter =object :ArrayAdapter<String>(
             this.context!!, android.R.layout.simple_spinner_dropdown_item,
-            listOf( "Male", "Female")
-        )
+            listOf("Male", "Female","Gender")
+        ){
+            override fun getCount(): Int = 2
+        }
+        gender_spinner.setSelection(2)
+        gender_spinner.prompt = "Gender"
+        birthday_picker.maxDate = Calendar.getInstance().timeInMillis
+
+
     }
 
 }
