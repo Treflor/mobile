3eca1a9f2b9d4b3988f5c46c6f48d23dc4394479
hiranthar <sahanhirantha@gmail.com>
2019-12-23 18:54:08 +0530

    journey doa added

--- app/src/main/java/com/treflor/data/db/TreflorDatabase.kt
+++ app/src/main/java/com/treflor/data/db/TreflorDatabase.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.treflor.data.db.dao.JourneyDao
 import com.treflor.data.db.dao.UserDao
+import com.treflor.models.Journey
 import com.treflor.models.User
 
 @Database(
-    entities = [User::class],
+    entities = [User::class, Journey::class],
     version = 1,
     exportSchema = false
 )
 abstract class TreflorDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+    abstract fun journeyDao(): JourneyDao
 
     companion object {
         @Volatile
---
+++ app/src/main/java/com/treflor/data/db/dao/JourneyDao.kt
@@ -0,0 +1,21 @@
+package com.treflor.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.treflor.models.CURRENT_JOURNEY_PK
+import com.treflor.models.Journey
+
+@Dao
+interface JourneyDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(journey: Journey)
+
+    @Query("SELECT * FROM journey WHERE pk = $CURRENT_JOURNEY_PK")
+    fun getUser(): Journey
+
+    @Query("DELETE FROM journey WHERE pk = $CURRENT_JOURNEY_PK")
+    fun delete()
+
+}
