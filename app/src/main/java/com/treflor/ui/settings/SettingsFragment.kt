f05f8f5ef1545937ecbe86ae722e825150f586db
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2020-02-18 18:10:42 +0530

    Recreated Settings fragment and layout

---
+++ app/src/main/java/com/treflor/ui/settings/SettingsFragment.kt
@@ -0,0 +1,39 @@
+package com.treflor.ui.settings
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+import com.treflor.R
+import kotlinx.android.synthetic.main.fragment_settings.*
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SettingsFragment : Fragment(), KodeinAware {
+
+    override val kodein: Kodein by closestKodein()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+}
